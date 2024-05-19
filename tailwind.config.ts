d2d500d037d6f605f73ccdeb53b8004cd308fa7c
Faruks23 <mdfaruksp2@gmail.com>
2024-05-19 10:56:33 +0600

    make changes 1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,10 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
-  
-   plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require("daisyui")],
 };
 export default config;
